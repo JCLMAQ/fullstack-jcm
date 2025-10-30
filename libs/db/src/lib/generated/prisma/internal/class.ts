e6cf3636ebd2006fffb47ac73620bc850a75a8d5
JCM <jcl.maquinay@gmail.com>
2025-10-30 14:51:03 +0100

    fix(env): mise à jour des paramètres de base de données et ajustement des chemins dans la configuration Prisma

--- libs/db/src/lib/generated/prisma/internal/class.ts
+++ libs/db/src/lib/generated/prisma/internal/class.ts
@@ -22,12 +22,12 @@ const config: runtime.GetPrismaClientConfig = {
       "value": "prisma-client"
     },
     "output": {
-      "value": "/Users/jean-claudemaquinay/Programmation/Monorepo/fullstack-jcm/libs/db/src/lib/generated/prisma",
+      "value": "/Users/jcm/Programmation/Monorepo/fullstack-jcm/libs/db/src/lib/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
-      "importFileExtension": "ts",
       "generatedFileExtension": "ts",
+      "importFileExtension": "ts",
       "engineType": "library"
     },
     "binaryTargets": [
@@ -38,7 +38,7 @@ const config: runtime.GetPrismaClientConfig = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/Users/jean-claudemaquinay/Programmation/Monorepo/fullstack-jcm/libs/db/src/lib/prisma/schema.prisma",
+    "sourceFilePath": "/Users/jcm/Programmation/Monorepo/fullstack-jcm/libs/db/src/lib/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativePath": "../../prisma",
